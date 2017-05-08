c39e75ff31965d9f924b03f641f5b26bf4180573
joaojordao <joao.jordao97@gmail.com>
2017-05-08 20:17:54 +0100

    altitude

--- Telemetria/pychart_test_jordao/app.py
+++ Telemetria/pychart_test_jordao/app.py
@@ -4,8 +4,13 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
  
-@app.route("/")
-def chart():
+
+@app.route('/homepage.html')
+def homepage():
+	return render_template('homepage.html')	
+
+@app.route("/temperature.html")
+def temp():
 	temp_file = open('temp.txt', 'r')
 	time_file = open('time.txt', 'r')
 	temp = []
@@ -19,7 +24,24 @@ def chart():
 	temp_file.close()
 	time_file.close()
 
-	return render_template('chart.html', values=temp, labels=time)
+	return render_template('temperature.html', values=temp, labels=time)
+
+@app.route("/altitude.html")
+def alt():
+	alt_file = open('alt.txt', 'r')
+	time_file = open('time.txt', 'r')
+	alt = []
+	time = []
+	for line_1 in alt_file:
+		alt = line_1.split()
+
+	for line_2 in time_file:
+		time = line_2.split()
+
+	alt_file.close()
+	time_file.close()
+
+	return render_template('altitude.html', values=alt, labels=time)
  
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=5001)
